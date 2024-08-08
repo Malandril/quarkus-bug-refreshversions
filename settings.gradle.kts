9df433791700b58462e59d05db29ce4c1be01f40
Thomas Canava <thomas.canava@soprasteria.com>
2024-08-08 20:40:47 +0200

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    val quarkusPluginVersion: String by settings
+    val quarkusPluginId: String by settings
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+        mavenLocal()
+    }
+    plugins {
+        id(quarkusPluginId) version quarkusPluginVersion
+    }
+}
+rootProject.name="quarkus-con"
+
+plugins {
+    id("de.fayard.refreshVersions") version "0.60.5"
+}
